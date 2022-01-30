3b9de6b5157c74fdd88c3ca56f895f17eaa474a0
Luca <lucamendez25112003@gmail.com>
2022-01-30 17:58:46 -0300

    el mate dulce, es malardo

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("aguante el mate wachin");
+	alert("el mate es amargo");
 }
 
