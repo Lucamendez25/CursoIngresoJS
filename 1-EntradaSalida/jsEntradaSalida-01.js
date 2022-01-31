53e323de7a0b30e7bab59ad8527e8b0622fb9ac3
Luca <lucamendez25112003@gmail.com>
2022-01-31 00:59:55 -0300

    mensaje para ian X2

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("El mate amargo, es lo mejor que hay");
+	alert("Ian se va a formosa, re tonto");
 }
 
