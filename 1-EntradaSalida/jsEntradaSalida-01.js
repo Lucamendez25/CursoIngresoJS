ed5ff1b6270e1273b90366601226f6f5f1f05027
Luca <lucamendez25112003@gmail.com>
2022-01-31 00:58:12 -0300

    mensaje para ian

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("cebame un matemático");
+	alert("El mate amargo, es lo mejor que hay");
 }
 
